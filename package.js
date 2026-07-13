3f54d1d7aaec85d73aa9d8defe7c8b413cb423c4
Daniel <danielx3.at@gmail.com>
2026-07-13 23:23:31 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "hugosmp-ticket-vouch-bot",
+  "version": "1.0.0",
+  "description": "Discord bot with a ticket system and a vouch system for HugoSMP Market",
+  "main": "index.js",
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "dotenv": "^16.4.5"
+  }
+}
